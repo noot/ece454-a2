69129a45865dff8f7491d4a45a945c3fe69d528c
noot <elizabethjbinks@gmail.com>
2019-10-01 14:03:57 -0400

    remove reference

--- implementation.c
+++ implementation.c
@@ -5,6 +5,15 @@
 #include "utilities.h"  // DO NOT REMOVE this line
 #include "implementation_reference.h"   // DO NOT REMOVE this line
 
+unsigned char *processMoveUp(unsigned char *buffer_frame, unsigned width, unsigned height, int offset);
+unsigned char *processMoveDown(unsigned char *buffer_frame, unsigned width, unsigned height, int offset);
+unsigned char *processMoveLeft(unsigned char *buffer_frame, unsigned width, unsigned height, int offset);
+unsigned char *processMoveRight(unsigned char *buffer_frame, unsigned width, unsigned height, int offset);
+unsigned char *processRotateCW(unsigned char *buffer_frame, unsigned width, unsigned height, int offset);
+unsigned char *processRotateCCW(unsigned char *buffer_frame, unsigned width, unsigned height, int offset);
+unsigned char *processMoveMirrorX(unsigned char *buffer_frame, unsigned width, unsigned height, int offset);
+unsigned char *processMoveMirrorY(unsigned char *buffer_frame, unsigned width, unsigned height, int offset);
+
 /***********************************************************************************************************************
  * @param buffer_frame - pointer pointing to a buffer storing the imported 24-bit bitmap image
  * @param width - width of the imported 24-bit bitmap image
@@ -17,10 +26,8 @@
 unsigned char *processMoveUp(unsigned char *buffer_frame, unsigned width, unsigned height, int offset) {
     // handle negative offsets
     if (offset < 0){
-        return processMoveDownReference(buffer_frame, width, height, offset * -1);
+        return processMoveDown(buffer_frame, width, height, offset * -1);
     }
-
-    //return processMoveUpReference(buffer_frame, width, height, offset);
 
     memmove(buffer_frame, buffer_frame+offset*width*3, width*3*height - width*3*offset);
 
@@ -40,7 +47,7 @@ unsigned char *processMoveUp(unsigned char *buffer_frame, unsigned width, unsign
 unsigned char *processMoveRight(unsigned char *buffer_frame, unsigned width, unsigned height, int offset) {
     // handle negative offsets
     if (offset < 0){
-        return processMoveLeftReference(buffer_frame, width, height, offset * -1);
+        return processMoveLeft(buffer_frame, width, height, offset * -1);
     }
 
     //return processMoveRightReference(buffer_frame, width, height, offset);
@@ -67,7 +74,7 @@ unsigned char *processMoveRight(unsigned char *buffer_frame, unsigned width, uns
 unsigned char *processMoveDown(unsigned char *buffer_frame, unsigned width, unsigned height, int offset) {
     // handle negative offsets
     if (offset < 0){
-        return processMoveUpReference(buffer_frame, width, height, offset * -1);
+        return processMoveUp(buffer_frame, width, height, offset * -1);
     }
 
     //return processMoveDownReference(buffer_frame, width, height, offset);
@@ -90,7 +97,7 @@ unsigned char *processMoveDown(unsigned char *buffer_frame, unsigned width, unsi
 unsigned char *processMoveLeft(unsigned char *buffer_frame, unsigned width, unsigned height, int offset) {
     // handle negative offsets
     if (offset < 0){
-        return processMoveRightReference(buffer_frame, width, height, offset * -1);
+        return processMoveRight(buffer_frame, width, height, offset * -1);
     }
 
     //return processMoveLeftReference(buffer_frame, width, height, offset);
@@ -117,7 +124,7 @@ unsigned char *processRotateCW(unsigned char *buffer_frame, unsigned width, unsi
                                int rotate_iteration) {
     // handle negative offsets
     if (rotate_iteration < 0){
-        return processRotateCCWReference(buffer_frame, width, height, rotate_iteration * -1);
+        return processRotateCCW(buffer_frame, width, height, rotate_iteration * -1);
     }
 
     if (rotate_iteration%4 == 1) {
